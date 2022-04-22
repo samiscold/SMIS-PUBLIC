369b23e8e05954beadac30f86ce8165c884b1fec
BS <bs47577@ubt-uni.net>
2022-04-22 18:44:20 +0200

    Client-side init

---
+++ client-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Student Management Information System</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
