941e9e72f74f8129afe9c8614795765c9c1c0db1
samiscold <62956581+samiscold@users.noreply.github.com>
2022-06-20 12:10:30 +0200

    Update NavBar.tsx

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -14,9 +14,11 @@ export default observer(function NavBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link as={NavLink} to='/countries'>Countries</Nav.Link>
-                        <NavLink to='/createCountry'><Button variant="success" >Create Country</Button></NavLink>
+                        <NavLink to='/createCountry'><Button variant="success btn-sm" >Create Country</Button></NavLink>
                         <Nav.Link as={NavLink} to='/departments'>Departments</Nav.Link>
-                        <NavLink to='/createDepartment'><Button variant="success" >Create Departments</Button></NavLink>
+                        <NavLink to='/createDepartment'><Button variant="success btn-sm" >Create Departments</Button></NavLink>
+                        <Nav.Link as={NavLink} to='/subjects'>Subjects</Nav.Link>
+                        <NavLink to='/createSubject'><Button variant="success btn-sm" >Create Subject</Button></NavLink>   
                     </Nav>
                 </Navbar.Collapse>
             </Container>
