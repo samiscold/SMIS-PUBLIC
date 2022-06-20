53a418fe73e9831fb90a85becc7ade8a791864b3
samiscold <62956581+samiscold@users.noreply.github.com>
2022-06-20 12:15:31 +0200

    Update DataContext.cs

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,6 @@ namespace Persistence
         
         public DbSet<Country> Countries { get; set; }
         public DbSet<Department> Departments { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
     }
 }
