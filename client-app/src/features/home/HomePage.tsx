e453f116bc23237e2fe8063b01af2186252e8ab4
samiscold <62956581+samiscold@users.noreply.github.com>
2022-06-21 09:44:17 +0200

    added ubt-logo

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -5,7 +5,16 @@ export default function HomePage () {
     return (
         <div className="masthead text-white">
             <Container>
-                <h1 className="mb-5">SMIS</h1>
+            <img
+                    alt="ubt-logo"
+                    src={require('../../app/images/ubt-logo.png')}
+                    width="60"
+                    height="60"
+                    className="d-inline-block"
+                    />
+
+                <h1 className="mb-2">SMIS</h1>
+               
                 <h2 className="mb-4">Welcome to Student Management Information System!</h2>
                 <Link to='/countries'>
                     <Button variant="btn btn-outline-light">
